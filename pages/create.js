d878556ea43fb3417359a35034f3fe0b81627ccf
Ayush Jain <ayushjain2205@gmail.com>
2023-05-30 19:24:51 +0530

    Add icons

--- pages/create.js
+++ pages/create.js
@@ -3,8 +3,11 @@ import React from "react";
 const Create = () => {
   return (
     <div className="mt-[18px] mx-[16px]">
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-between items-center">
         <h5 className="text-[32px]">Create ...</h5>
+        <div className="flex flex-row h-[48px] w-[48px] border-[2px] border-black rounded-full items-center justify-between">
+          <img className="m-auto" src="menu.svg" alt="" />
+        </div>
       </div>
     </div>
   );
