fdd9d5691d124a30a032404e94e081cd96e363f1
Ayush Jain <ayushjain2205@gmail.com>
2023-06-01 15:40:25 +0530

    appearcnce ui

--- pages/appearance.js
+++ pages/appearance.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import Menu from "../components/Menu";
 
 const appearance = () => {
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileInputChange = (event) => {
+    const selectedFile = event.target.files[0];
+    // Do something with the selected file
+    console.log("Selected file:", selectedFile);
+  };
+
   return (
     <div className="mt-[18px] mx-[16px]">
       <div className="flex flex-row justify-between items-center mb-[20px]">
@@ -19,6 +31,26 @@ const appearance = () => {
           The picture they view when they claim it, view it on the map and
           moree..
         </p>
+      </div>
+      <div className="flex flex-row gap-[14px] mt-[28px]">
+        <div>
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileInputChange}
+            style={{ display: "none" }}
+          />
+          <button
+            className="flex flex-row gap-[8px] justify-center items-center w-[148px] h-[48px] bg-black text-white rounded-[8px]"
+            onClick={handleButtonClick}
+          >
+            <img src="upload.svg" alt="" />
+            Upload
+          </button>
+        </div>
+        <button className="flex flex-row gap-[8px] justify-center items-center w-[148px] h-[48px] bg-black text-white rounded-[8px]">
+          <img src="camera-white.svg" alt="" /> Take video
+        </button>
       </div>
     </div>
   );
