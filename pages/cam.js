b2d2e5845af29034cb2614aaf10225a3fb7bbca3
Ayush Jain <ayushjain2205@gmail.com>
2023-06-01 11:40:16 +0530

    Add camera view component

---
+++ pages/cam.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import Cameraview from "../components/Cameraview";
+import Link from "next/link";
+
+const Cam = () => {
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setShowImage(true);
+    }, 7000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+  return (
+    <div>
+      <div style={{ zIndex: 10000000, position: "absolute" }}>
+        <Link href="/celebration">
+          {showImage && (
+            <img
+              className="animate-spin fixed top-[250px] left-[100px] h-[202px] w-[207px]"
+              alt=""
+              src="/marker1.png"
+            />
+          )}
+        </Link>
+      </div>
+      <div style={{ zIndex: 0 }}>
+        <Cameraview />
+      </div>
+    </div>
+  );
+};
+
+export default Cam;
